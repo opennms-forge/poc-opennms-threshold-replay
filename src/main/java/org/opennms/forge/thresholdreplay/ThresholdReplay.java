f6ab5e06fd2c2e0f66f85a9ea59d55489dd8ee1b
Markus Neumann <Markus@OpenNMS.org>
2013-07-19 01:47:40 +0200

    added ThresholdReplay to hold the data about one ThresholdReplay, including jodatime

--- src/main/java/org/opennms/forge/thresholdreplay/ThresholdReplay.java
+++ src/main/java/org/opennms/forge/thresholdreplay/ThresholdReplay.java
@@ -1,6 +1,9 @@
 package org.opennms.forge.thresholdreplay;
 
 import java.util.List;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +28,32 @@ public class ThresholdReplay {
 
     public List<ThresholdOccur> getThresholdOccurs() {
         return thresholdOccurs;
+    }
+    
+    public String toFormatedString() {
+        String result = "\n";
+        result = result.concat(thresholdConfiguration.toFormatedString() + "\n");
+        Period overallPeriod = new Period();
+        for (ThresholdOccur thresholdOccur : thresholdOccurs) {
+            overallPeriod = overallPeriod.plus(thresholdOccur.getPeriod());
+        }
+        result = result.concat("\t Threshold exeeded " + thresholdOccurs.size() + " times for overall " + formatPeriod(overallPeriod) + "\n");
+        return result;
+    }
+    
+    //TODO find a better place for this pice
+    private static String formatPeriod(Period period) {
+        PeriodFormatter daysHoursMinutes = new PeriodFormatterBuilder()
+                .appendDays()
+                .appendSuffix(" day", " days")
+                .appendSeparator(" and ")
+                .appendMinutes()
+                .appendSuffix(" minute", " minutes")
+                .appendSeparator(" and ")
+                .appendSeconds()
+                .appendSuffix(" second", " seconds")
+                .toFormatter();
+        return daysHoursMinutes.print(period);
     }
     
 }
